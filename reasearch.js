88edfdbdba59f657c032ba25707df303b85f82a0
EmbrunForestier <germain.poullot@uni-osnabrueck.de>
2025-09-30 11:41:22 +0200

    Add files via upload

--- reasearch.js
+++ reasearch.js
@@ -1,13 +1,13 @@
 function ShowHide(k) {
   var x = document.getElementById("Abstract" + k);
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "";
   } else {
     x.style.display = "none";
   }
   var x = document.getElementById("show_button" + k);
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "";
   } else {
     x.style.display = "none";
   }
